ced764f0df2797ba50fc36aa7370bbce655d2a80
JDomCamZ <juansehermanito@gmail.com>
2023-01-03 03:01:41 -0500

    Reading csv file

---
+++ Balanceador-Java/src/main/java/Balanceador.java
@@ -0,0 +1,151 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Balanceador {
+    TCPServer50 mTcpServer;
+    Scanner sc;
+    BlockingQueue<String> blockingQueue = new LinkedBlockingDeque<>(5);
+    BlockingQueue<String> consumingQueue = new LinkedBlockingDeque<>(5);
+    ArrayList<Integer> client = new ArrayList<Integer>();
+    ArrayList<Integer> segment = new ArrayList<Integer>();
+    static int[][] data = new int[1000][2];
+    boolean ready = false;
+    boolean readyOther=false;
+    public static void main(String[] args) throws InterruptedException {
+        File file = new File("Balanceador-Java", "src");
+        file = new File(file, "main");
+        file = new File(file, "resources");
+        file = new File(file, "bd.csv");
+        String filePath = file.getAbsolutePath();
+        System.out.println(filePath);
+        String delimiter = ";";
+        int row = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(delimiter);
+                for (int col = 0; col < 2; col++) {
+                    data[row][col] = Integer.parseInt(fields[col]);
+                }
+                row++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // print the array to check the data
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        Balanceador objser = new Balanceador();
+        objser.iniciar();
+    }
+    void iniciar() throws InterruptedException {
+        new Thread(
+                new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mTcpServer = new TCPServer50(
+                                new TCPServer50.OnMessageReceived(){
+                                    @Override
+                                    public void messageReceived(String message) throws InterruptedException {
+                                            ServidorRecibe(message);
+                                    }
+                                }
+                        );
+                        mTcpServer.run();
+                    }
+                }
+        ).start();
+        //-----------------
+        String salir = "n";
+        sc = new Scanner(System.in);
+        System.out.println("Servidor bandera 01");
+        while( !salir.equals("s")){
+            salir = sc.nextLine();
+            //ServidorEnvia(salir);
+        }
+        System.out.println("Servidor bandera 02");
+    }
+    void ServidorRecibe(String llego) throws InterruptedException {
+        String[] t =llego.split("tt");
+        System.out.println(data[0][0]);
+        if(t[0].equals("c")){
+            ProducerRecibe(t[1]);
+        }else{
+            if (llego.equals("Producer")) {
+                AddProducer(mTcpServer.IDClient());
+            }
+            else if (llego.equals("Consumer"))
+                AddSegment(mTcpServer.IDClient());
+            else if (!llego.equals("listo") && blockingQueue.remainingCapacity()!=0)
+                blockingQueue.put(llego);
+            if (blockingQueue.remainingCapacity()==0) {
+                String cola = "COLA LLENA ESPERA POR FAVOR";
+                System.out.println(cola);
+                mTcpServer.sendProducerMessageTCPServer(cola, client.get(0));
+            }
+            if (llego.equals("listo"))
+                ready = true;
+            System.out.println("SERVIDOR40 El mensaje:" + llego);
+            if ((ready==true && segment.size()>0 && client.size()>0) && !blockingQueue.isEmpty()) {
+                ready = false;
+                //System.out.println(blockingQueue.remainingCapacity());
+                ServidorEnvia("s");
+            }
+
+        }
+
+
+
+    }
+
+    void ProducerRecibe(String llego) throws InterruptedException {
+       if (!llego.equals("listo") && consumingQueue.remainingCapacity()!=0)
+           consumingQueue.put(llego);
+        if (consumingQueue.remainingCapacity()==0) {
+            String cola = "COLA LLENA ESPERA POR FAVOR";
+            System.out.println(cola);
+            mTcpServer.sendProducerMessageTCPServer(cola, segment.get(0));
+        }
+        if (llego.equals("listo"))
+            readyOther = true;
+        System.out.println("SERVIDOR40 El mensaje:" + llego);
+        if ((readyOther==true && segment.size()>0 && client.size()>0) && !consumingQueue.isEmpty()) {
+            readyOther = false;
+            //System.out.println(consumingQueue.remainingCapacity());
+            ConsumingEnvia("s");
+        }
+    }
+    void ServidorEnvia(String sus) throws InterruptedException {
+        String envia = blockingQueue.take();
+        if (mTcpServer != null) {
+            mTcpServer.sendConsumingMessageTCPServer(envia, segment,-1);
+        }
+    }
+    void ConsumingEnvia(String sus) throws InterruptedException {
+        String enviaP = consumingQueue.take();
+        if (mTcpServer != null) {
+            mTcpServer.sendConsumingMessageTCPServer(enviaP, client,-1);
+        }
+    }
+    void AddProducer (int ID) {
+        client.add(ID);
+    }
+
+    void AddSegment(int ID) {
+        segment.add(ID);
+    }
+}
---
+++ Balanceador-Java/src/main/java/TCPServer50.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class TCPServer50 {
+    private String message;
+    int nrcli = 0;
+    public static final int SERVERPORT = 4444;
+    private OnMessageReceived messageListener = null;
+    private boolean running = false;
+    TCPServerThread50[] sendclis = new TCPServerThread50[10];
+    PrintWriter mOut;
+    BufferedReader in;
+    ServerSocket serverSocket;
+    //el constructor pide una interface OnMessageReceived
+    public TCPServer50(OnMessageReceived messageListener) {
+        this.messageListener = messageListener;
+    }
+    public OnMessageReceived getMessageListener(){
+        return this.messageListener;
+    }
+    public void sendConsumingMessageTCPServer(String message, ArrayList<Integer> consumers, int a){
+        if(a == -1) {
+            for (int i = 0; i < consumers.size(); i++) {
+                sendclis[consumers.get(i)].sendMessage(message);
+                System.out.println("ENVIANDO A CONSUMING " + (i + 1));
+            }
+        }else{
+            sendclis[a].sendMessage(message);
+            System.out.println("ENVIANDO A CONSUMING " + (a + 1));
+        }
+    }
+    public void sendProducerConsuming(int index) {
+        sendclis[index].sendMessage("Is Segment or Client?");
+        System.out.println("ENVIANDO A CLIENTE " + (index));
+    }
+
+    public void sendProducerMessageTCPServer(String message, int indexProducer){
+        sendclis[indexProducer].sendMessage(message);
+        System.out.println("ENVIANDO A PRODUCER " + (indexProducer));
+    }
+
+    public int IDClient(){
+        return nrcli;
+    }
+    
+    public void run(){
+        running = true;
+        try{
+            System.out.println("TCP Server"+"S : Connecting...");
+            serverSocket = new ServerSocket(SERVERPORT);
+            
+            while(running){
+                Socket client = serverSocket.accept();
+                System.out.println("TCP Server"+"S: Receiving...");
+                nrcli++;
+                System.out.println("Engendrado " + nrcli);
+                sendclis[nrcli] = new TCPServerThread50(client,this,nrcli,sendclis);
+                Thread t = new Thread(sendclis[nrcli]);
+                t.start();
+                System.out.println("Nuevo conectado:"+ nrcli+" jugadores conectados");
+                TimeUnit.SECONDS.sleep(5);
+                sendProducerConsuming(nrcli);
+            }
+            
+        }catch( Exception e){
+            System.out.println("Error"+e.getMessage());
+        }finally{
+
+        }
+    }
+    public  TCPServerThread50[] getClients(){
+        return sendclis;
+    } 
+
+    public interface OnMessageReceived {
+        public void messageReceived(String message) throws InterruptedException;
+    }
+}
