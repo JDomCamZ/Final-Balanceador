ac4ee259db92918871aadfbd62b5afad19a618f8
ArmandoLluen <92061855+ArmandoLluen@users.noreply.github.com>
2023-01-09 08:10:13 -0500

    cliente avance

--- Cliente-Python/cliente.py
+++ Cliente-Python/cliente.py
@@ -3,10 +3,10 @@ import sys
 import threading
 import time
 import random
+import numpy as np
 
 ip = '192.168.1.37'
 port = 4444
-mensaje = ""
 id_cliente = 1
 mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mi_socket.connect((ip, port))
@@ -20,14 +20,6 @@ def recibir():
         print("se recibio: " + respuesta)
 
 
-def working():
-    timetosleep = random.random() * 3 + 1
-    time.sleep(timetosleep)
-    mensaje = 'listo\n'
-    mi_socket.send(mensaje.encode())
-    print("termino el trabajo")
-
-
 def send(mensaje):
     mi_socket.send(mensaje.encode())
     if mensaje == "adios\n":
@@ -37,15 +29,15 @@ def send(mensaje):
 
 # C--01-L-672
 def lectura():
-    mensaje = "C--" + "0" + str(id_cliente) + "-L-" + str(random.randint(1, 1000))
+    mensaje = "C--" + "0" + str(id_cliente) + "-L-" + str(random.randint(1, 1000000))
     print(mensaje)
     send(mensaje)
 
 
 # C--02-A-420;730;30.20
 def actualizar():
-    mensaje = "C--" + "0" + str(id_cliente) + "-A-" + str(random.randint(1, 1000))
-    mensaje = mensaje + ";" + str(random.randint(1, 1000))+";" + str(round(random.random()*1000, 2))
+    mensaje = "C--" + "0" + str(id_cliente) + "-A-" + str(random.randint(1, 1000000))
+    mensaje = mensaje + ";" + str(random.randint(1, 1000000))+";" + str(round(random.random()*1000, 2))
     print(mensaje)
     send(mensaje)
 
@@ -54,7 +46,7 @@ mensaje = 'listo\n'
 send(mensaje)
 respuesta = mi_socket.recv(4000).decode()
 
-if (respuesta == "Is Segment or Client?\r\n"):
+if respuesta == "Is Segment or Client?\r\n":
     mensaje = 'Client\n'
     send(mensaje)
     print("Esperando mensaje del balanceador")
@@ -62,12 +54,17 @@ if (respuesta == "Is Segment or Client?\r\n"):
     respuesta = mi_socket.recv(4000).decode()
     splits = respuesta.split()
 
-    if (splits[0] == 'Cliente'):
+    if splits[0] == 'Cliente':
         id_cliente = splits[1]
         print("el numero de cliente es: " + id_cliente)
     threadRecib = threading.Thread(name="h1", target=recibir, args=())
     threadRecib.start()
 
-lectura()
-actualizar()
-
+for i in range(100000):
+    nRand = np.random.choice([0, 1], 100, p=[0.6, 0.4])
+    nRand = random.choice(nRand)
+    if nRand == 0:
+        lectura()
+    else:
+        actualizar()
+    time.sleep(1/10)
