cbfe4c8adeb7600b6f161dec0713590ccaeea4c9
MiguelHRLX <miguel.huamani.r@uni.pe>
2023-01-09 19:31:56 -0500

    UPDATE

--- Segmento-C#/Segmento/Program.cs
+++ Segmento-C#/Segmento/Program.cs
@@ -24,10 +24,10 @@ namespace Segmento
             IPEndPoint remoteEP = new IPEndPoint(ipAddress, 4444);
 
             //replica
-            IPAddress ipReplica = IPAddress.Parse("192.168.0.18");//192.168.0.18
-            IPEndPoint remoteReplica = new IPEndPoint(ipReplica, 5000);
-            Socket senderReplica = new Socket(ipReplica.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            senderReplica.Connect(remoteReplica);
+            //IPAddress ipReplica = IPAddress.Parse("192.168.0.18");//192.168.0.18
+            //IPEndPoint remoteReplica = new IPEndPoint(ipReplica, 5000);
+            //Socket senderReplica = new Socket(ipReplica.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //senderReplica.Connect(remoteReplica);
             
 
             LeerCSV(bd);
@@ -37,7 +37,7 @@ namespace Segmento
                 Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
                 // Conecta el socket al punto de conexión remoto.
                 sender.Connect(remoteEP);
-                senderReplica.Connect(remoteReplica);
+                //senderReplica.Connect(remoteReplica);
 
                 Console.WriteLine("Socket conectado a {0}", sender.RemoteEndPoint.ToString());
                 
@@ -54,7 +54,8 @@ namespace Segmento
                     int byteRec = sender.Receive(bytes);
                     string texto = Encoding.ASCII.GetString(bytes, 0, byteRec);
                     Console.WriteLine("Servidor:" + texto);
-                    Operaciones(texto,bd,sender,senderReplica);
+                    //Operaciones(texto,bd,sender,senderReplica);
+                    Op(texto, bd, sender);
                 }
 
                 sender.Shutdown(SocketShutdown.Both);
@@ -98,7 +99,8 @@ namespace Segmento
         static void EnviarMensaje(Socket sender,string mensaje)
         {
             Console.WriteLine("Enviando mensajes");
-            byte[] msg = Encoding.ASCII.GetBytes(mensaje + "<EOF>");
+            //byte[] msg = Encoding.ASCII.GetBytes(mensaje + "<EOF>");
+            byte[] msg = Encoding.ASCII.GetBytes(mensaje);
             int byteSent = sender.Send(msg);
         }
         static void EnviarReplica(Socket replica, string mensaje)
@@ -117,7 +119,8 @@ namespace Segmento
                         int max = int.Parse(partesOperación[1]);
                         limite[0] = min;
                         limite[1] = max;
-                        EnviarMensaje(repli, texto);
+                        
+                        //EnviarMensaje(repli, texto);
                     }
                     if (partesOperación[0] == "L"){
                             string dinero = "";
@@ -172,7 +175,8 @@ namespace Segmento
                             Console.WriteLine(resultado);
                             EnviarMensaje(sender, resultado);
                             //enviar a replica
-                            EnviarMensaje(repli, texto);
+                            
+                            //EnviarMensaje(repli, texto);
                         }
                           }
                         
@@ -181,6 +185,92 @@ namespace Segmento
 
                 }
   
+        }
+
+        static void Op(string texto, List<string[]> datos, Socket sender)
+        {
+            string[] partesOperación = texto.Split("-");
+            if (partesOperación.Length > 1)
+            {
+                if (partesOperación[0] == "B")
+                {
+                    int min = int.Parse(partesOperación[0]);
+                    int max = int.Parse(partesOperación[1]);
+                    limite[0] = min;
+                    limite[1] = max;
+
+                    //EnviarMensaje(repli, texto);
+                }
+                if (partesOperación[0] == "L")
+                {
+                    string dinero = "";
+                    string resultado = "";
+                    string busca = partesOperación[1];
+                    foreach (string[] dato in datos)
+                    {
+                        if (dato[0] == busca) dinero = dato[1];
+                    }
+                    resultado = "L-" + busca + "-" + dinero + partesOperación[2];
+                    EnviarMensaje(sender, resultado);
+                }
+                else if (partesOperación[0] == "A")
+                {
+                    string[] partesActualizar = partesOperación[2].Split(";");
+                    float monto = float.Parse(partesActualizar[2]);
+                    string ordenante = partesActualizar[0];
+                    string beneficiario = partesActualizar[1];
+                    foreach (string[] dato in datos)
+                    {
+                        if (dato[0] == ordenante)
+                        {
+                            float dineroActual = float.Parse(dato[1]);
+                            float resultadoDinero = (dineroActual - monto);
+                            if (resultadoDinero < 1000)
+                            {
+                                string resultado = "A-D-" + ordenante + "-" + partesOperación[2];
+                                Console.WriteLine(resultado);
+                                EnviarMensaje(sender, resultado);
+                            }
+                            else
+                            {
+                                string resultado = "A-A-" + ordenante + "-" + resultadoDinero.ToString() + "-" + beneficiario + "-" + partesActualizar[2] + "-" + partesOperación[2];
+                                Console.WriteLine(resultado);
+                                EnviarMensaje(sender, resultado);
+                            }
+                        }
+                    }
+
+                }
+                else if (partesOperación[0] == "R")
+                {
+                    string[] Actualizar = partesOperación[1].Split(";");
+                    string beneficiario = Actualizar[0];
+                    float dineroDepositado = float.Parse(Actualizar[1]);
+
+                    foreach (string[] dato in datos)
+                    {
+                        if (dato[0] == beneficiario)
+                        {
+                            float dinero = float.Parse(dato[1]);
+                            float dineroActualizado = dinero + dineroDepositado;
+
+                            dato[1] = dineroActualizado.ToString();
+                            Escribir(dato[0] + ";" + dato[1], int.Parse(dato[0]));
+
+                            string resultado = "R-" + beneficiario + "-" + (dineroActualizado.ToString()) + "-" + partesOperación[2];
+                            Console.WriteLine(resultado);
+                            EnviarMensaje(sender, resultado);
+                            //enviar a replica
+
+                            //EnviarMensaje(repli, texto);
+                        }
+                    }
+
+
+                }
+
+            }
+
         }
 
         static void LeerCSV(List<string[]>lista)
