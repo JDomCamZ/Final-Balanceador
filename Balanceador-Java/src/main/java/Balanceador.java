d8759455b3e94026b74c98f14eae409b1e494eb0
JDomCamZ <juansehermanito@gmail.com>
2023-01-09 23:26:37 -0500

    Trying something.

--- Balanceador-Java/src/main/java/Balanceador.java
+++ Balanceador-Java/src/main/java/Balanceador.java
@@ -18,10 +18,14 @@ public class Balanceador {
     int balance;
     public static void main(String[] args) throws InterruptedException {
         //Leer datos de BBDD en .csv
-        File file = new File("Segmento-C#", "Segmento");
+        /*File file = new File("Segmento-C#", "Segmento");
         file = new File(file, "bin");
         file = new File(file, "Debug");
         file = new File(file, "netcoreapp3.1");
+        file = new File(file, "bd.csv");*/
+        File file = new File("Balanceador-Java", "src");
+        file = new File(file, "main");
+        file = new File(file, "resources");
         file = new File(file, "bd.csv");
         String filePath = file.getAbsolutePath();
         System.out.println(filePath);
